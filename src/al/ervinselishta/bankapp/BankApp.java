4222965b144e160c1312c8f852e515f44bc8829d
ervinselishta <ervinselishta@hotmail.com>
2023-03-02 20:05:46 +0100

    no message

---
+++ src/al/ervinselishta/bankapp/BankApp.java
@@ -0,0 +1,12 @@
+package al.ervinselishta.bankapp;
+
+public class BankApp {
+
+	public static void main(String[] args) {
+
+		Account ervin = new Account("Ervin Selishta", "J00928103W");
+		ervin.showmenu();
+
+	}
+
+}
